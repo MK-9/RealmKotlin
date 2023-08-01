5e03e294749c29cebbb602566c1050ea5ad0030f
soltanmohammadi <msit26055@gmail.com>
2023-08-01 17:04:53 +0330

    insert data into db

--- app/src/main/java/com/example/mykotlinrealm/MainActivity.kt
+++ app/src/main/java/com/example/mykotlinrealm/MainActivity.kt
@@ -5,17 +5,20 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import io.realm.Realm
 import io.realm.RealmConfiguration
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         Realm.init(this)
-        RealmConfiguration.Builder()
+        val config = RealmConfiguration.Builder()
             .name("library")
-            .schemaVersion(1)
+            .schemaVersion(0)
             .migration { realm, oldVersion, newVersion ->
 
                 Log.d("aaa", "DefaultRealmDB: oldVersion:$oldVersion")
@@ -25,5 +28,33 @@ class MainActivity : AppCompatActivity() {
             }
             .build()
 
+        Realm.setDefaultConfiguration(config)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            for (account in getAccount()) {
+                withContext(Dispatchers.Main) {
+                    Log.d("Result: Account", "${account.title}")
+                }
+            }
+        }
     }
+
+    private suspend fun initAccount() {
+        val accountDao = DefaultAccountDao()
+        accountDao.insert(1, "a")
+        accountDao.insert(2, "b")
+        accountDao.insert(3, "c")
+        accountDao.insert(4, "d")
+        accountDao.insert(5, "e")
+        accountDao.insert(6, "f")
+        accountDao.insert(7, "g")
+        accountDao.insert(8, "h")
+    }
+
+    private suspend fun getAccount(): List<Account> {
+        val accountDao = DefaultAccountDao()
+        return accountDao.getAccount()
+    }
+
+
 }
