3fd4e1f12f558c383920d4e380b25f9b81a2fb9b
soltanmohammadi <msit26055@gmail.com>
2023-08-01 19:04:58 +0330

    change account entity

--- app/src/main/java/com/example/mykotlinrealm/Account.kt
+++ app/src/main/java/com/example/mykotlinrealm/Account.kt
@@ -7,12 +7,15 @@ open class Account(
     @PrimaryKey
     var id: Int = 0,
     var userId: Int = 0,
-    var title: String? = null
+    var title: String? = null,
+    var subtitle: String? = null
 ) : RealmObject() {
 
     companion object {
+        const val ACCOUNT_CLASS = "Account"
         const val COL_ID = "id"
         const val COL_USER_ID = "userId"
         const val COL_TITLE = "title"
+        const val COL_SUBTITLE = "subtitle"
     }
 }
